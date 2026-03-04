2bb46865c66128361b8b5cf9a247d86ac316a0f3
Aishwaryasugumar <aishwaryasugumar91@gmail.com>
2026-03-04 12:15:05 +0530

    removed only

--- yes/accordian.spec.ts
+++ yes/accordian.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.only('verify Accordian', async ({ page }) => {
+test('verify Accordian', async ({ page }) => {
   await page.goto('https://www.webdriveruniversity.com/Accordion/index.html');
   await page.locator("#manual-testing-accordion").click()
   const text=await page.locator('//div[@id="manual-testing-description"]/p')
