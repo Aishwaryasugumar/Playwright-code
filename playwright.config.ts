563b339fc2cc8907c139a76878c69dd3fdae79be
Aishwaryasugumar <aishwaryasugumar91@gmail.com>
2026-03-09 11:27:58 +0530

    Report

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,10 +13,11 @@ dotenv.config();//->to load env file
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  reporter: [['html', { open: 'never' }]],
   testDir: './yes',
   expect: {
   timeout: 20 * 1000,
-},
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -26,7 +27,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
